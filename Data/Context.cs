7eb9da42a1b20e0dd54701941b26877f2d64e5e9
Matheus Maria <mmatheus.maria@gmail.com>
2019-07-16 22:37:52 -0300

    Context Start

---
+++ Data/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace SuportAPI.Data
+{
+    public partial class BaseContext : DbContext
+    {
+        
+    }
+
+    public class TicketContext : BaseContext
+    {
+        public DbSet<Ticket> Tickets { get; set; }
+        
+    }
+}
